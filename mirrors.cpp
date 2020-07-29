7c6a1b38779327b6cc5ee2dc21f7ad0c502f6493
hy <haiyang@gmail.com>
2020-07-28 21:56:14 -0400

    july 28 2020

--- mirrors.cpp
+++ mirrors.cpp
@@ -17,9 +17,36 @@ int main()
     for (int i=0; i<N; i++) {
         int w, z; char c;
         cin>>w>>z>>c;
+        x[w].push_back(z);
+        y[z].push_back(w);
         ii temp; temp.first=w, temp.second=z;
         m[temp]=c;
         ml.push_back(temp);
     }
+    long sum=0;
+    int upp=min((2*K)+1, max_dist);
+    for (int i=1; i<=upp; i++) {
+        if (u.count(i)==1) {
+            sum+=u[i];
+        }
+    }
+    if (upp==max_dist) {
+        cout<<sum<<'\n';
+        return 0;
+    }
+    long ans=sum;
+    for (int i=2; i<=max_dist; i++) {
+        if (u.count(i-1)==1) {
+            sum-=u[i-1];
+        }
+        if ((2*K)+i <= max_dist) {
+            if (u.count((2*K)+i)==1) {
+                sum+=u[(2*K)+i];
+            }
+        }
+        //cout<<sum<<'\n';
+        if (sum>ans) ans=sum;
+    }
+    cout<<ans<<'\n';
     
 }
---
+++ nqueen.cpp
@@ -0,0 +1,33 @@
+#include <fstream>
+using namespace std;
+
+int n, queen[100];
+
+int check(int y, int x)
+{
+    for (int i=0; i<y; i++) {
+        if (i==y || queen[i]==x || i-y==queen[i]-x || i-y==x-queen[i]) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int rec(int y)
+{
+    if (y>=n) return 1;
+    int ans=0;
+    for (int i=0; i<n; i++) {
+        if (!check(y, i)) {
+            queen[y]=i;
+            ans+=rec(y+1);
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    cin>>n;
+    cout<<rec(0)<<'\n';
+}
