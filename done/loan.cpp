e0a9d55af4a4d7ccf85ed35ed877e866290b6186
hy <haiyang@gmail.com>
2020-11-11 22:57:02 -0500

    nov 11 2020

---
+++ done/loan.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+#define ii pair<int, int>
+#define ll long long
+#define vi vector<int>
+#define vc vector<char>
+#define pb push_back
+#define rz resize
+#define mp make_pair
+#define f first
+#define s second
+
+#define FOR(n) for (int i = 0; i < n; i++)
+#define FOR1(n) for (int i = 1; i <= n; i++)
+#define RFOR(n) for (int i = n - 1; i >= 0; i--)
+#define DFOR(n, m) FOR(n) for (int j = 0; j < m; j++)
+
+int dx[4] = {1, 0, -1, 0}; 
+int dy[4] = {0, 1, 0, -1};
+int ddx[8] = {1, 0, -1, -1, -1, 0, 1, 1};
+int ddy[8] = {1, 1, 1, 0, -1, -1, -1, 0};
+
+bool in_bounds(int x, int y, int n, int m) {
+    if (x >= 0 && x < n && y >= 0 && y < m) return true;
+    return false;
+}
+
+ll n, k, m;
+
+int valid(ll x, ll k) {
+  ll g = 0;
+  while(k > 0 && g < n) {
+    ll y = (n - g) / x;
+    if(y < m) {
+      ll leftover = (n-g + m-1) / m;
+      return leftover <= k;
+    }
+    ll maxmatch = n - x*y;
+    ll numdays = (maxmatch - g) / y + 1;
+    if(numdays > k) numdays = k;
+    g += y * numdays;
+    k -= numdays;
+  }
+  return g >= n;
+}
+ 
+
+int main() {
+    freopen("loan.in", "r", stdin);
+    freopen("loan.out", "w", stdout);
+    scanf("%lld%lld%lld", &n, &k, &m);
+    //cout<<"n = "<<n<<endl;
+
+    ll l = 1, r = 1e12;
+    while (l < r) {
+        //cout<<"l = "<<l<<", r = "<<r<<endl;
+        long long mid = l + r + 1 >> 1;
+        if (valid(mid, k)) l = mid;
+        else r = mid - 1;
+    }
+    cout<<l<<endl;
+    return 0;
+}
